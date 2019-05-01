097fc77cb35156015a102dd5dd383074df73f109
ASTIC <ASTIC@hotmail.de>
2019-05-01 18:22:00 +0200

    local nuget packet

--- EmpyrionNetAPIDefinitions/Properties/AssemblyInfo.cs
+++ EmpyrionNetAPIDefinitions/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("EmpyrionAPIDefinitions")]
+[assembly: AssemblyTitle("EmpyrionNetAPIDefinitions")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("EmpyrionAPIDefinitions")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyProduct("EmpyrionNetAPIDefinitions")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
