d7e49722aef49d58287640d678a32c231e96e036
ASTIC <ASTIC@hotmail.de>
2019-06-21 18:21:44 +0200

    No timout exception when no wait

--- EmpyrionNetAPIAccess/TaskExtensions.cs
+++ EmpyrionNetAPIAccess/TaskExtensions.cs
@@ -14,20 +14,26 @@ namespace EmpyrionNetAPIAccess
 
         public static async Task<TResult> For<TResult>(TimeSpan timeout, Task<TResult> task)
         {
-            if(timeout.Ticks == 0) return await task;
-
             using (var timeoutCancellationTokenSource = new CancellationTokenSource())
             {
-
-                var completedTask = await Task.WhenAny(task, Task.Delay(timeout, timeoutCancellationTokenSource.Token));
-                if (completedTask == task)
+                try
                 {
-                    timeoutCancellationTokenSource.Cancel();
-                    return await task;  // Very important in order to propagate exceptions
+                    var completedTask = await Task.WhenAny(task, Task.Delay(timeout.Ticks == 0 ? new TimeSpan(0, 0, 1) : timeout, timeoutCancellationTokenSource.Token));
+                    if (completedTask == task)
+                    {
+                        timeoutCancellationTokenSource.Cancel();
+                        return await task;  // Very important in order to propagate exceptions
+                    }
+                    else
+                    {
+                        if(timeout.Ticks == 0) return await Task.FromResult(default(TResult));
+                        throw new TimeoutException("The operation has timed out.");
+                    }
                 }
-                else
+                catch (TimeoutException)
                 {
-                    throw new TimeoutException("The operation has timed out.");
+                    if(timeout.Ticks > 0) throw;
+                    return await Task.FromResult(default(TResult));
                 }
             }
         }
