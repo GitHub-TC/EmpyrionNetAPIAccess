b1fbc7398f7c42008eb13cfe04c3a205fe309422
Brett Caswell <brcaswell@hotmail.com>
2020-02-14 06:20:01 -0600

    update ExampleModFramework and Tests, update package reference to 5.0.5 usage, add NLog package, handle for Broker extension APIRequest refactor changes

--- ExampleModFramework/ExampleModFramework.cs
+++ ExampleModFramework/ExampleModFramework.cs
@@ -1,56 +1,189 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Eleon.Modding;
 using EmpyrionNetAPIAccess;
 using EmpyrionNetAPIDefinitions;
+using EmpyrionNetAPITools;
+using NLog;
+using NLog.Common;
+using NLog.Config;
+using NLog.Targets;
+using UnityEngine;
+using NLogLevel = NLog.LogLevel;
 
 namespace ExampleMod
 {
-    public class ExampleMod : EmpyrionModBase
+    [Serializable]
+    public class GameEventInfo
     {
+        public CmdId EventId { get; set; }
+        public ushort SequenceNumber { get; set; }
+        public DateTime Updated { get; set; }
+        public ulong GameTick { get; set; }
+    }
+
+    [Serializable]
+    public class GameEventPayload : GameEventInfo
+    {
+        public object Data { get; set; }
+    }
+
+    public class NlogMemoryTarget : Target
+    {
+        public event EventHandler<string> OnLog;
+
+        public NlogMemoryTarget(string name, NLogLevel level) : this(name, level, level) { }
+        public NlogMemoryTarget(string name, NLogLevel minLevel, NLogLevel maxLevel)
+        {
+            // important: we want LogManager.Configuration property assign behaviors \ magic to occur
+            //   see: https://stackoverflow.com/a/3603571/1366179
+            var config = LogManager.Configuration ?? new XmlLoggingConfiguration(string.Join(@"\\", Path.Combine(EmpyrionConfiguration.ModPath, "ExampleModFramework" ), "NLog.config")); // new LoggingConfiguration(new LogFactory());
+            
+            // Add Target and Rule to their respective collections
+            config.AddTarget(name, this);
+            config.LoggingRules.Add(new LoggingRule("*", minLevel, maxLevel, this));
+
+            LogManager.Configuration = config;
+        }
+
+        [Obsolete]
+        protected override void Write(AsyncLogEventInfo[] logEvents)
+        {
+            foreach (var logEvent in logEvents)
+            {
+                Write(logEvent.LogEvent);
+            }
+        }
+
+        protected override void Write(AsyncLogEventInfo logEvent)
+        {
+            Write(logEvent.LogEvent);
+        }
+
+        protected override void Write(LogEventInfo logEvent)
+        {
+            OnLog(this, string.Join(" ", $"-{logEvent.Level.Name.ToUpper()}-", logEvent.FormattedMessage));
+        }
+
+        // consider overriding WriteAsyncThreadSafe methods as well.
+    }
+
+    public class ExampleMod : IMod, ModInterface
+    {
+        private static Logger _nlog = LogManager.GetCurrentClassLogger();
+
+        private Broker broker = new Broker();
+
+        private IModApi _modApi;
+        private NlogMemoryTarget _nlogMemoryTarget;
+
+        private DateTime _lastUpdate;
+        //private GameEventInfo _lastGameEventInfo;
 
         readonly Random rnd = new Random();
 
-
-        public override void Initialize(ModGameAPI dediAPI)
+        public ExampleMod()
         {
+        }
 
-            this.Update_Received += ExampleMod_Update_Received;
-            this.Event_ChatMessage += ExampleMod_Event_HandleLottoChatMessage;
-            this.Event_GameEvent += ExampleMod_Event_GameEvent;
-            this.Event_Statistics += PlayerDied_Event_Statistics;
-            this.ChatCommands.Add(new ChatCommand(@"/repeat (?<repeat>\S+)", ChatCommand_TestMessage));
-            this.ChatCommands.Add(new ChatCommand(@"!loudly (?<yellthis>.+)", async (data, args) =>
-            {
-                var msg = new IdMsgPrio()
-                {
-                    id = data.playerId,
-                    msg = $"{args["yellthis"].ToUpper()}!!!!!"
-                };
-                await Request_InGameMessage_SinglePlayer(msg);
-            }));
+        void IMod.Init(IModApi modAPI)
+        {
+            _modApi = modAPI;
+#if DEBUG
+            _nlogMemoryTarget = new NlogMemoryTarget("ExampleMod", NLogLevel.Trace, NLogLevel.Fatal);
+#else
+            _nlogMemoryTarget = new NlogMemoryTarget("ExampleMod", NLogLevel.Trace, NLogLevel.Fatal);
+#endif
+            _nlogMemoryTarget.OnLog += _nlogMemoryTarget_OnLog;
 
-            this.ChatCommands.Add(new ChatCommand(@"/explosion", async (data, __) =>
-            {
-                var dialogData = new DialogBoxData()
-                {
-                    Id = data.playerId,
-                    MsgText = "BOOM!",
-                    PosButtonText = "yes",
-                    NegButtonText = "No"
-                };
+            _modApi.Application.OnPlayfieldLoaded += Application_OnPlayfieldLoaded;
+        }
 
-                var t = Request_ShowDialog_SinglePlayer(dialogData);
-                t.Wait();
-                var resultInterpreted = t.Result.Value == 0 ? "YES" : "NO";
+        private void _nlogMemoryTarget_OnLog(object sender, string e) => _modApi.Log(e);
 
-                await Request_InGameMessage_SinglePlayer(resultInterpreted.ToIdMsgPrio(data.playerId));
-            }, "blows it up", PermissionType.Moderator));
+        void IMod.Shutdown()
+        {
+            _nlog.Trace("Shutting Down");
+        }
 
+        public void Game_Update()
+        {
+            _lastUpdate = DateTime.UtcNow;
 
+            //_nlog.Trace($" _modApi?.Application?.GameTicks := { _modApi?.Application?.GameTicks ?? 0 }");
+            //_nlog.Trace($" Broker?.api?.Game_GetTickTime() := { broker?.api?.Game_GetTickTime() ?? 0 }");
+
+            if (TryGetBrokerApiGameTicks(out var tick))
+                ExampleMod_Update_Received(tick);
+        }
+
+        private bool TryGetBrokerApiGameTicks(out ulong tick)
+        {
+            tick = broker?.api?.Game_GetTickTime() ?? 0;
+            return tick > 0;
+        }
+
+        private bool TryGetModApiGameTicks(out ulong tick)
+        {
+            tick = _modApi?.Application?.GameTicks ?? 0;
+            return tick > 0;
+        }
+
+        public void Initialize(ModGameAPI dediAPI)
+        {
+            _nlog.Trace("Calling Initialize!");
+            broker.api = dediAPI;
+
+            if (dediAPI != null)
+                _nlog.Trace("dediAPI (ModGameAPI legacy) exists");
+
+            broker.Event_Playfield_Loaded += Broker_Event_Playfield_Loaded;
+
+            //Broker.Update_Received += ExampleMod_Update_Received;
+            //Broker.Event_ChatMessage += ExampleMod_Event_HandleLottoChatMessage;
+            //Broker.Event_GameEvent += ExampleMod_Event_GameEvent;
+            //Broker.Event_Statistics += PlayerDied_Event_Statistics;
+
+            //ChatCommands.AddRange(new[]
+            //{
+            //    new ChatCommand(@"/repeat (?<repeat>\S+)", RepeatMessageHandlerAsync),
+            //    new ChatCommand(@"!loudly (?<yellthis>.+)", YellThisHandlerAsync),
+            //    new ChatCommand(@"/explosion", ExplosionHandlerAsync, "blows it up", PermissionType.Moderator)
+            //});
+
+            //async Task RepeatMessageHandlerAsync(ChatInfo data, Dictionary<string, string> args) =>
+            //    await broker.Request_InGameMessage_SinglePlayer(new IdMsgPrio
+            //    {
+            //        id = data?.playerId ?? 0,
+            //        msg = string.Format("{0} {0} {0}!", args["repeat"])
+            //    });
+
+            //async Task YellThisHandlerAsync(ChatInfo data, Dictionary<string, string> args) =>
+            //    await broker.Request_InGameMessage_SinglePlayer(new IdMsgPrio()
+            //    {
+            //        id = data?.playerId ?? 0,
+            //        msg = $"{args["yellthis"]?.ToUpper()}!!!!!"
+            //    });
+
+            //async Task ExplosionHandlerAsync(ChatInfo data, Dictionary<string, string> args)
+            //{
+            //    var playerId = data?.playerId ?? 0;
+
+            //    var result = (await broker.SendRequestAsync<DialogBoxData, IdAndIntValue>(CmdId.Request_ShowDialog_SinglePlayer, new DialogBoxData
+            //    {
+            //        Id = playerId,
+            //        MsgText = "BOOM!",
+            //        PosButtonText = "yes",
+            //        NegButtonText = "No"
+            //    })).Value == 0 ? "YES" : "NO";
+                
+            //    await broker.SendRequestAsync(CmdId.Request_InGameMessage_SinglePlayer, result.ToIdMsgPrio(playerId));
+            //}
 
             //this.ChatCommands.Add(new ChatCommand(@"/help", (data, __) => {
             //  this.Request_Player_Info(data.playerId.ToId(), (info) =>
@@ -76,20 +209,30 @@ namespace ExampleMod
             //}));
         }
 
-        private async Task ChatCommand_TestMessage(ChatInfo data, Dictionary<string, string> args)
+        private void Application_OnPlayfieldLoaded(IPlayfield playfield)
         {
-            var repeating = args["repeat"];
-            var msg = new IdMsgPrio()
+            _nlog.Trace($"Application_OnPlayfieldLoaded Raised => PlayfieldLoad := \"{playfield.Name}\"");
+            foreach (var p in playfield.Players)
             {
-                id = data.playerId,
-                msg = $"{repeating} {repeating} {repeating}!"
-            };
-            await Request_InGameMessage_SinglePlayer(msg);
+                var player = p.Value;
+                var vessel = player.Structure;
+                var dockedVessels = vessel?.GetDockedVessels() ?? new List<IStructure>();
+
+                _nlog.Trace("Player {0} is Piloting {1} and has {2} vessels docked", player.Name, vessel?.Entity.Name ?? "Nothing", dockedVessels.Count());
+            }
         }
 
-        private void PlayerDied_Event_Statistics(StatisticsParam obj)
+        private void Broker_Event_Playfield_Loaded(PlayfieldLoad obj)
         {
-            Log("***************event statistics!!!");
+            _nlog.Trace($"Broker_Event_Playfield_Loaded Raised => PlayfieldLoad := \"{obj.playfield}\"");
+            
+
+            //broker.api.Game_Request(CmdId.Request_InGameMessage_SinglePlayer, )
+        }
+
+        private async void PlayerDied_Event_Statistics(StatisticsParam obj)
+        {
+            //Log("***************event statistics!!!");
             var container = StatisticsContainer.FromStatisticsParam(obj);
 
             switch (container)
@@ -99,29 +242,30 @@ namespace ExampleMod
                     {
                         msg = $"Player {deathStats.PlayerId.id} was killed by {deathStats.KillerId.id}"
                     };
-                    Log(msg.msg);
-                    this.Request_InGameMessage_AllPlayers(msg).Wait();
+                    broker.Log(msg.msg);
+                    await broker.SendRequestAsync(CmdId.Request_InGameMessage_AllPlayers, msg);
                     break;
                 default:
                     break;
             }
         }
 
-        private void ExampleMod_Event_GameEvent(GameEventData obj)
+        private async void ExampleMod_Event_GameEvent(GameEventData obj)
         {
-            Log("*****************OMG it's a game event!!!");
-            var eventMessage = new IdMsgPrio
+            //Log("*****************OMG it's a game event!!!");
+            _nlog.Trace($@"A game event occured, name:{obj.Name}, type:{obj.Type}, eventType:{obj.EventType}");
+            
+            await broker.SendRequestAsync(CmdId.Request_InGameMessage_AllPlayers, new IdMsgPrio
             {
                 msg = $@"A game event occured, name:{obj.Name}, type:{obj.Type}, eventType:{obj.EventType}"
-            };
-
-            this.Request_InGameMessage_AllPlayers(eventMessage).Wait();
+            });
         }
 
         private void ExampleMod_Event_HandleLottoChatMessage(ChatInfo obj)
         {
-            Log("lotto check");
-            if (obj.msg != "lottery") return;
+            //Log("lotto check");
+            _nlog.Trace("lotto check");
+            if ((obj?.msg ?? string.Empty) != "lottery") return;
 
             //this.Request_Player_List(list =>
             //{
@@ -166,17 +310,55 @@ namespace ExampleMod
 
         }
 
+
         private async void ExampleMod_Update_Received(ulong tick)
         {
-            if (tick % 10000 == 0)
-            {
-                var param = new IdMsgPrio()
-                {
-                    msg = $"game tick is now {tick}, an auspicious number"
-                };
 
-                await this.Request_InGameMessage_AllPlayers(param);
+            if (tick != 0 && tick % 200 == 0)
+            {
+                //using (var source = new CancellationTokenSource(3000)) //1 seconds                
+                //{
+                var startTime = DateTime.UtcNow;
+                var msg = $"{startTime.ToString("yyyy-MM-dd hh:mm:ss.fff")} :: game tick is now {tick}, an auspicious number";
+                _nlog.Info($"broker.Request_InGameMessage_AllPlayers => msg := {msg}");
+                try
+                {
+                    var r = await broker.Request_InGameMessage_AllPlayers(new IdMsgPrio { msg = msg });
+                    //var r = await broker.Request_InGameMessage_AllPlayers(new IdMsgPrio { msg = msg }, source.Token);
+
+                    //var r = await broker.SendRequestAsync(CmdId.Request_InGameMessage_AllPlayers, new IdMsgPrio { msg = msg }).ConfigureAwait(false);
+                    //var r = await broker.SendRequestAsync(CmdId.Request_InGameMessage_AllPlayers, new IdMsgPrio { msg = msg }, source.Token).ConfigureAwait(false);
+
+                    _nlog.Info($" After {DateTime.UtcNow.Subtract(startTime).Milliseconds} ms => Task Completed := {r}");
+                }
+                catch (Exception ex) when (ex is OperationCanceledException || ex is TaskCanceledException)
+                {
+                    _nlog.Error($" After {DateTime.UtcNow.Subtract(startTime).Milliseconds} ms :: {ex?.Message ?? "Task was Cancelled"}");
+                }
+                //}
             }
+        }
+
+        public void Game_Start(ModGameAPI dediAPI)
+        {
+            Initialize(dediAPI);
+            _nlog.Trace("Game_Start called!");
+        }
+
+        public void Game_Exit()
+        {
+            _nlog.Trace("Game_Exit called!");
+        }
+
+        public void Game_Event(CmdId eventId, ushort seqNr, object data) => Game_Event(new GameEventPayload { EventId = eventId, SequenceNumber = seqNr, Data = data, GameTick = 0, Updated = DateTime.UtcNow });
+    
+        public void Game_Event(GameEventPayload gameEventInfo)
+        {            
+            _nlog.Trace($"Game_Event Called! {Environment.NewLine}{ JsonUtility.ToJson(gameEventInfo, true) }");
+            if (broker.HandleGameEvent(gameEventInfo.EventId, gameEventInfo.SequenceNumber, gameEventInfo.Data))
+                _nlog.Info("broker => Game_Event Handled");
+            else
+                _nlog.Info("broker => Game_Event Unhandled");
         }
     }
 }
